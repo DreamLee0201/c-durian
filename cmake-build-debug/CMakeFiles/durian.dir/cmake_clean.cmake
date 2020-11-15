c453fc5bf9d88c5c928dc09e0ccb2c850409e45f
DreamLee <595394202@qq.com>
2020-11-15 10:15:19 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/durian.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/durian.dir/main.cpp.obj"
+  "durian.exe"
+  "durian.exe.manifest"
+  "durian.pdb"
+  "libdurian.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/durian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = sizeof(char);
+    cout << a << endl;
+    return 0;
+}
